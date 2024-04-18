892acf8f75b1deefa3ff553cade93a0e1c09c28b
Charles Mendes <charlesluizmendes@gmail.com>
2024-04-18 18:29:56 -0300

    Adicionando Logs do Pipeline

--- src/Crud/Program.cs
+++ src/Crud/Program.cs
@@ -42,6 +42,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 // Prometheus
 
 var counter = Metrics.CreateCounter("webapimetric", "Counts requests to the WebApiMetrics API endpoints",
